59a60972a159fb147a108e5be2d1981314df8e38
Leticia <lzduque@hotmail.com>
2020-03-02 19:31:23 -0500

    decoded and created types

--- app/Main.hs
+++ app/Main.hs
@@ -3,26 +3,17 @@ module Main where
 
 import Lib (someFunc)
 import qualified Data.ByteString.Lazy as B
-import Data.Aeson (FromJSON,ToJSON)
+import Data.Aeson (FromJSON,ToJSON,Value,decode)
 import GHC.Generics (Generic)
 
 -- main function that is running everything
 main :: IO ()
 main = do
   str <- getJSON
-  B.putStrLn str
-
--- type of the input that it's being parsed
-data Requirements =
-  Requirements { season       :: String
-                , style       :: String
-                , bodyType    :: String
-                , numOutfits  :: Int
-                } deriving (Show,Generic)
-
--- Aeson methods to parse
-instance FromJSON Requirements
-instance ToJSON Requirements
+  let decodedStr = decode str :: Maybe WishJSON
+  case decodedStr of
+    Just wish -> print wish
+    Nothing -> print "nothing"
 
 -- importing a json file
 jsonFile :: FilePath
@@ -32,19 +23,34 @@ jsonFile = "test-1.json"
 getJSON :: IO B.ByteString
 getJSON = B.readFile jsonFile
 
+-- instance FromJSON WishJSON where
+--   parseJSON = withObject "WishJSON" $
 
--- instance FromJSON Requirements where
---  parseJSON (Object v) =
---     Person <$> v .: "season"
---            <*> v .: "style"
---            <*> v .: "bodyType"
---            <*> v .: "numOutfits"
---  parseJSON _ = mzero
+-- -------------- // TYPES // --------------
+-- type of the input that it's being parsed
+data WishJSON =
+  WishJSON { season       :: String
+            , style       :: String
+            , bodyType    :: String
+            , numOutfits  :: Int
+            } deriving (Show,Generic)
 
--- instance ToJSON Requirements where
---  toJSON (Person season style bodyType numOutfits) =
---     object [ "season"     .= season
---            , "style"      .= style
---            , "bodyType"   .= bodyType
---            , "numOutfits" .= numOutfits
---              ]
+-- Aeson methods to parse
+instance FromJSON WishJSON
+instance ToJSON WishJSON
+
+-- data Wish =
+--   Wish { season       :: Season
+--         , style       :: Style
+--         , bodyType    :: BodyType
+--         , numOutfits  :: Int
+--         } deriving (Show,Generic)
+
+data Season = Spring | Summer | Autumn | Winter
+  deriving Show
+
+data Style = Casual | Office
+  deriving Show
+
+data BodyType = Rectangle | Apple | InvertedTriangle | Hourglass | Triangle
+  deriving Show
