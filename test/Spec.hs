26afdbcd2d047a6f453e86e8b5d9a6246c0055fe
Leticia <lzduque@hotmail.com>
2020-03-25 19:31:51 -0400

    refactor

--- test/Spec.hs
+++ test/Spec.hs
@@ -9,31 +9,32 @@ import Wish
   )
 import Data.Aeson (decode)
 
+decodeJSON :: FilePath -> IO (Maybe Wish)
+decodeJSON filePath = do
+  str <- getJSON filePath
+  return $ decode str
+
 main :: IO ()
 main = hspec $ do
   describe "decode" $ do
     it "returns a Wish given a JSON file with extra content" $ do
-      str <- getJSON "test-1.json"
-      let decodedStr = decode str :: Maybe Wish
-      decodedStr `shouldBe` Just (Wish {season = AutumnWinter, style = Casual})
+      wish <- decodeJSON "test-1.json"
+      wish `shouldBe` Just (Wish {season = AutumnWinter, style = Casual})
 
   describe "decode" $ do
     it "returns nothing given a JSON file with wrong content - style" $ do
-      str <- getJSON "test-2.json"
-      let decodedStr = decode str :: Maybe Wish
-      decodedStr `shouldBe` Nothing
+      wish <- decodeJSON "test-2.json"
+      wish `shouldBe` Nothing
 
   describe "decode" $ do
     it "returns nothing given a JSON file with wrong content - season" $ do
-      str <- getJSON "test-3.json"
-      let decodedStr = decode str :: Maybe Wish
-      decodedStr `shouldBe` Nothing
+      wish <- decodeJSON "test-3.json"
+      wish `shouldBe` Nothing
 
   describe "decode" $ do
     it "returns nothing given a JSON file with no content" $ do
-      str <- getJSON "test-4.json"
-      let decodedStr = decode str :: Maybe Wish
-      decodedStr `shouldBe` Nothing
+      wish <- decodeJSON "test-4.json"
+      wish `shouldBe` Nothing
 
     -- it "returns the first element of an *arbitrary* list" $
     --   property $ \x xs -> head (x:xs) == (x :: Int)
