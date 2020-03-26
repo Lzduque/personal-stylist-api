abfb0e3b76a90e0f533fd418540cf0f6b48dbb54
Leticia <lzduque@hotmail.com>
2020-03-26 15:11:05 -0400

    add choose capsule logic

--- app/Main.hs
+++ app/Main.hs
@@ -17,6 +17,14 @@ import Data.Aeson (decode)
 
 jsonFile = "test-1.json"
 
+-- LOGIC --
+chooseCapsule :: Wish -> CapsuleWardrobe
+chooseCapsule wish
+  | wish == Wish {season = AutumnWinter, style = Casual} = autumnWinterCasualCW
+  | wish == Wish {season = SpringSummer, style = Casual} = springSummerCasualCW
+  | wish == Wish {season = AutumnWinter, style = Office} = autumnWinterOfficeCW
+  | wish == Wish {season = SpringSummer, style = Office} = springSummerOfficeCW
+
 -- main function that is running everything
 main :: IO ()
 main = do
