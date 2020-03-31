55af7bf61b599fc9a5fd511fd484f191234f3d88
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2020-03-31 14:32:00 -0400

    adjust tests

--- src/Wish.hs
+++ src/Wish.hs
@@ -22,5 +22,15 @@ data NumberOfOutfits = From10to20 | From21to30 | From31to40 | From41to50 | From5
 data Colors = White | OffWhite | Beige | Brown | Black | Navy | Blue | LightBlue | DarkGreen | LightGreen | DarkYellow | LightYellow | DarkPink | LightPink | DarkRed | LightRed | DarkOrgange | LightOrange | DarkPurple | LightPurple
   deriving (Show, Eq, Generic, ToJSON, FromJSON)
 
--- data BodyType = Rectangle | Apple | InvertedTriangle | Hourglass | Triangle
---   deriving Show
+-- data BodyType = Rectangle | Circle | InvertedTriangle | Hourglass | Triangle
+--   deriving (Show, Eq, Generic, ToJSON, FromJSON)
+
+-- Hourglass
+--   General 
+--     "Try to choose light fabrics that don't add too much volume."
+--     "The best fabric for your body type is soft but firm and elastic, such as viscose, polyester, jersey, silk, chiffon, soft wool crepe, fine knits and fabrics with lycra."
+--   Dresses
+--     "Try acentuating your curves, drawing attention to your waist."
+--     "The best dresses are wrap, bodycon, monocrome, with V-neckline, but never too tight."
+--   Tops
+--     ""
